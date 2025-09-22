0363e0f0056796f262294cebe297396384a99236
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-22 20:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let btn=document.getElementById('button')
+let counter=documnet.getElementById('counter')
+
+let value=0
+
+counter.textContent=value
+
+btn.addEventListener("onClick",()=>{
+	value+=1
+	counter.textContent=value
+})
